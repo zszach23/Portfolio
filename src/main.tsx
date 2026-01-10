4d9e931e3739034c1b2da1578a6ee3aab9c0b1d0
Zach Sally <zszach23@gmail.com>
2026-01-10 00:15:16 -0500

    [DEP] Added TailwindCSS

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@
 // Entry point for the React application.
 // ============================================================================
 
+import '../index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppProvider from './common/providers/AppProvider'
