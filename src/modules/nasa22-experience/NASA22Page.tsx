5e113d9f2846e8b74bc520b14facc1d9d9b0b1cb
Zach Sally <zszach23@gmail.com>
2026-02-10 21:26:00 -0500

    [FIX] Update NASA-22 Page to Use Layout

--- src/modules/nasa22-experience/NASA22Page.tsx
+++ src/modules/nasa22-experience/NASA22Page.tsx
@@ -4,20 +4,12 @@
 // Page component for the NASA 22 Experience
 // ============================================================================
 
-import ExperienceHero from "../../common/components/experience-pages/ExperienceHero";
 import { NASA22Summary } from "../../content/experiences/work/nasa-22/NASA22Summary";
 import { NASA22Content } from "../../content/experiences/work/nasa-22/NASA22Content";
-import ExperienceOverviewSection from "../../common/components/experience-pages/ExperienceOverviewSection";
-import ExperienceContributionsSection from "../../common/components/experience-pages/ExperienceContributionsSection";
-import ExperienceChallengesSection from "../../common/components/experience-pages/ExperienceChallengesSection";
+import ExperiencePage from "../../common/components/experience-pages/ExperiencePage";
 
 export default function NASA22Page() {
     return (
-        <div className="">
-            <ExperienceHero bannerImageUrl={NASA22Content.bannerImageUrl} experienceSummary={NASA22Summary} />
-            <ExperienceOverviewSection overview={NASA22Content.overview} />
-            <ExperienceContributionsSection contributions={NASA22Content.contributions} />
-            <ExperienceChallengesSection challenges={NASA22Content.challenges} />
-        </div>
+        <ExperiencePage experienceSummary={NASA22Summary} experienceContent={NASA22Content} />
     )
 }
